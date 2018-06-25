74a86953cb88d265b7f23a0d1e0d418eb22742f5
Hoan Nguyen <nvhoanbk14@gmail.com>
2018-06-25 09:48:32 +0700

    update start method

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
+
 //Install express server
 const express = require('express');
+const http = require('http');
 const path = require('path');
-
 const app = express();
-
+const port = process.env.PORT || 4200; //  listening on the default Heroku port
+const server = http.createServer(app);
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+app.use(express.static(path.join(__dirname, '/dist')));
+///Start the app by
+app.all('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+app.set('port', port);
+server.listen(port, () => console.log('Running...'));
